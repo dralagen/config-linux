dc4c68e53a8eb3e8ca742036077459d2777e5b23
Dralagen <dralagen@dralagen.fr>
2015-01-26 19:14:16 +0100

    add config for slend

---
+++ i3/autostart.slend.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+wicd-client --tray 1>&2 2> /dev/null &
+
+start-pulseaudio-x11
+
+feh --bg-center $HOME/.background.png
+
+# pcmanfm --no-desktop -d &
+
+sudo gopenvpn &
+
+/home/dralagen/.i3/notifyBattery.sh &
+
+exit 0
+
