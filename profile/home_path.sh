e5567d372c7138bd54540a87bb623134efa60689
Dralagen <dralagen@dralagen.fr>
2015-02-24 18:09:23 +0100

    add profile for user executable dir

---
+++ profile/home_path.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#  /etc/profile.d/home_path.sh
+#
+
+# add ~/.scripts/ executable directly in bash
+export PATH=$PATH:$HOME/.scripts
+
+# add ~/.bin/ executable directly in bash
+export PATH=$PATH:$HOME/.bin
+
