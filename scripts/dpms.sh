d4762bd269b008ca9e9d4ae049fd1eee8a13e32d
Dralagen <dralagen@dralagen.fr>
2015-03-06 23:17:01 +0100

    disable xautolock when stop dpms

--- scripts/dpms.sh
+++ scripts/dpms.sh
@@ -4,13 +4,16 @@ case "$1" in
     off)
         xset -dpms
         xset s off
+        xautolock -disable 2 > /dev/null
         ;;
     now)
         xset dpms force off
+        xautolock -enable 2 > /dev/null
         ;;
     *)
         xset +dpms
         xset s on
+        xautolock -enable 2 > /dev/null
         ;;
 esac
 
